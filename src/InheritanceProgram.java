3a162ab0ee76760bfd4d74e4d91433db2d8b5437
malgorzata.majchrowi <maugosiam@gmail.com>
2018-11-18 14:48:55 +0100

    abc

--- src/InheritanceProgram.java
+++ src/InheritanceProgram.java
@@ -1,10 +1,6 @@
 public class InheritanceProgram {
     public static void main(String[] args) {
 
-        Circle circle = new Circle("circle", 10);
-        Triangle triangle = new Triangle("triangle", 7.16, 40);
-        Rectangle rectangle = new Rectangle("rectangle", 9, 1);
-
         Shapes[] figury = {
                 new Circle("circle", 10),
                 new Triangle("triangle", 10, 30),
@@ -17,14 +13,5 @@ public class InheritanceProgram {
             i.areaCalculation();
             System.out.println(i.getArea());
         }
-        /*System.out.printf("%.2f%n", calculateFigureArea(circle));
-        System.out.printf("%.2f%n", calculateFigureArea(triangle));
-        System.out.printf("%.2f%n", calculateFigureArea(rectangle));*/
     }
-
-    /*public static double calculateFigureArea(Shapes shape) {
-        shape.areaCalculation();
-        return shape.getArea();
-    }
-*/
 }
